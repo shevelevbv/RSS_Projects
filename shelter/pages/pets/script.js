8aadd25848788db32446a37b634245ade0bb9dfa
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-04-16 21:54:00 -0400

    feat: add menu eventlisteners for >=320px

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,11 +1,12 @@
 const burgerIcon = document.querySelector('.burger');
 const header = document.querySelector('.header');
 
-burgerIcon.addEventListener('click', toggleOpenClass);
+header.addEventListener('click', toggleOpenClass);
 
 function toggleOpenClass () {
   if (event.target.classList.contains('burger') ||
-  event.target.classList.contains('burger__line')) {
+  event.target.classList.contains('burger__line') || 
+  event.target.classList.contains('nav__link')) {
     header.classList.toggle('open');
   }
 }
