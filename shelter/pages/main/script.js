afa7b18a1d95cdccc2ecfbe922a104b93e06ab67
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-04-20 20:46:33 -0400

    feat: add sliding navigation menu for <768px

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,12 +1,15 @@
 const burgerIcon = document.querySelector('.burger');
 const header = document.querySelector('.header');
+const body = document.querySelector('body');
+const pageMask = document.querySelector('.page-mask');
 
 header.addEventListener('click', toggleOpenClass);
 
-function toggleOpenClass () {
+function toggleOpenClass (event) {
   if (event.target.classList.contains('burger') ||
   event.target.classList.contains('burger__line') || 
-  event.target.classList.contains('nav__link')) {
-    header.classList.toggle('open');
+  event.target.classList.contains('nav__link') ||
+  event.target.classList.contains('page-mask')) {
+    body.classList.toggle('open');
   }
 }
