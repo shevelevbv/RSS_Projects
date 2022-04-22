ea8555c4c5f6533d1f5d92a496ef79d86fb57c34
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-04-22 14:22:28 -0400

    feat: add carousel animation

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,15 +1,32 @@
-const burgerIcon = document.querySelector('.burger');
-const header = document.querySelector('.header');
-const body = document.querySelector('body');
-const pageMask = document.querySelector('.page-mask');
+const HEADER = document.querySelector('.header');
+const BODY = document.querySelector('body');
+const PET_SHOW = document.querySelector('.our-friends__show');
+const CARDS = document.querySelector('.our-friends__cards');
+const BTN_LEFT = document.querySelector('.our-friends__back');
+const BTN_RIGHT = document.querySelector('.our-friends__forward');
 
-header.addEventListener('click', toggleOpenClass);
+HEADER.addEventListener('click', toggleOpenClass);
+PET_SHOW.addEventListener('click', moveCards);
+CARDS.addEventListener('animationend', () => {
+  CARDS.classList.remove('move-left');
+  CARDS.classList.remove('move-right');
+  PET_SHOW.addEventListener('click', moveCards);
+})
+
+function moveCards (event) {
+  if (event.target.classList.contains('our-friends__back')) {
+    CARDS.classList.add('move-left');
+  } else if (event.target.classList.contains('our-friends__forward')) {
+    CARDS.classList.add('move-right');
+  }
+  PET_SHOW.removeEventListener('click', moveCards);
+}
 
 function toggleOpenClass (event) {
   if (event.target.classList.contains('burger') ||
   event.target.classList.contains('burger__line') || 
   event.target.classList.contains('nav__link') ||
   event.target.classList.contains('page-mask')) {
-    body.classList.toggle('open');
+    BODY.classList.toggle('open');
   }
 }
