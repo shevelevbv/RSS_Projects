b319e79b01d4fc8bac13c87e36488a3f1419450f
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-06-30 13:36:29 -0400

    feat: install node packages and add api key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '48c7410fecb344a7bf036b03f4aa47bc',
         });
     }
 }
