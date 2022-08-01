b23a93606bcfdd2a13bd1082032b0245c1136ee5
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-07-31 22:59:40 -0400

    feat: add input for choosing car's color

---
+++ async-race/src/helpers/functions.ts
@@ -0,0 +1,14 @@
+export default function createElement<T extends HTMLElement>(
+  parentElement: HTMLElement | null,
+  tagName: string,
+  classes = '',
+  content = '',
+): T {
+  const newElement: HTMLElement = document.createElement(tagName);
+  newElement.className = classes;
+  newElement.textContent = content;
+  if (parentElement) {
+    parentElement.append(newElement);
+  }
+  return newElement as T;
+}
--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+import './style.css';
+import createElement from './helpers/functions';
+
+const colorInput: HTMLInputElement = createElement(document.body, 'input', 'input__color');
+colorInput.type = 'color';
+
+const carContainer: HTMLDivElement = createElement(document.body, 'div', 'container');
+const carControls: HTMLDivElement = createElement(carContainer, 'div', 'car__controls');
+createElement(carControls, 'button', 'car__button_select', 'SELECT');
+createElement(carControls, 'button', 'car__button_remove', 'REMOVE');
+createElement(carControls, 'span', 'car__span_name', 'Tesla');
+const SVGContainer: HTMLDivElement = createElement(carContainer, 'div', 'car__svg_container');
+const car: SVGSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+car.setAttribute('class', 'icon car');
+const useSVG: SVGUseElement = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+useSVG.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '../img/sprite.svg#car');
+car.append(useSVG);
+SVGContainer.append(car);
+
+colorInput.addEventListener('change', () => {
+  car.style.fill = colorInput.value;
+});
